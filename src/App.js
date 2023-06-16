40f1b862a80ddcfba82108aacc64d39df22a25a3
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-16 12:43:54 +0200

    add list&listItem components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import "./App.css";
 import Form from "./components/Form/Form.js";
 import useLocalStorageState from "use-local-storage-state";
 import { uid } from "uid";
+import List from "./components/List/List";
 
 function App() {
-	const [activities, setActivities] = useLocalStorageState("activities", {
-		defaultValue: [],
-	});
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
 
-	function handleAddActivity(newActivity) {
-		newActivity = { ...newActivity, id: uid() };
-		setActivities([newActivity, ...activities]);
-	}
+  function handleAddActivity(newActivity) {
+    newActivity = { ...newActivity, id: uid() };
+    setActivities([newActivity, ...activities]);
+  }
 
-	return (
-		<div className="App">
-			<Form onAddActivity={handleAddActivity}></Form>
-		</div>
-	);
+  return (
+    <div className="App">
+      <List activities={activities} />
+      <Form onAddActivity={handleAddActivity} />
+    </div>
+  );
 }
 
 export default App;
