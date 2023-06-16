3ea6f301ca8d2fbe3370b9b1a03256d671c76784
Veliko <Kardziev>
2023-06-16 14:50:36 +0200

    add filter for weather

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import { uid } from "uid";
 import List from "./components/List/List";
 
 function App() {
-  const [activities, setActivities] = useLocalStorageState("activities", {
-    defaultValue: [],
-  });
+	const [activities, setActivities] = useLocalStorageState("activities", {
+		defaultValue: [],
+	});
 
-  function handleAddActivity(newActivity) {
-    newActivity = { ...newActivity, id: uid() };
-    setActivities([newActivity, ...activities]);
-  }
+	function handleAddActivity(newActivity) {
+		newActivity = { ...newActivity, id: uid() };
+		setActivities([newActivity, ...activities]);
+	}
+	const isGoodWeather = false;
+	const filteredActivities = activities.filter(
+		(activity) => activity.isForGoodWeather === isGoodWeather
+	);
 
-  return (
-    <div className="App">
-      <List activities={activities} />
-      <Form onAddActivity={handleAddActivity} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<List activities={filteredActivities} isGoodWeather={isGoodWeather} />
+			<Form onAddActivity={handleAddActivity} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,29 @@
 import "./List.style.css";
 import ListItem from "./ListItem/ListItem";
 
-export default function List({ activities }) {
-  return (
-    <ul className="list" aria-label="List of Activities">
-      {activities.map((activity) => {
-        return <ListItem>{activity.name}</ListItem>;
-      })}
-    </ul>
-  );
+export default function List({ activities, isGoodWeather }) {
+	if (activities.length === 0) {
+		return <></>;
+	} else {
+		return (
+			<section>
+				{isGoodWeather ? (
+					<p className="list__description">
+						The weather is awesome!
+						<br />
+						Go outside and:
+					</p>
+				) : (
+					<p className="list__description">
+						Bad weather outside! Here's what you can do now:
+					</p>
+				)}
+				<ul className="list" aria-label="List of Activities">
+					{activities.map((activity) => {
+						return <ListItem>{activity.name}</ListItem>;
+					})}
+				</ul>
+			</section>
+		);
+	}
 }
