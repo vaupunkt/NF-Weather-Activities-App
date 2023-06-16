dbd114cee28cb2c76bb1e0a5b2b8ddbb694cd454
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-16 11:06:07 +0200

    add localStorage State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Form from "./components/Form/Form.js";
 import useLocalStorageState from "use-local-storage-state";
+import { uid } from "uid";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
@@ -8,7 +9,9 @@ function App() {
   });
 
   function handleAddActivity(newActivity) {
-    console.log(newActivity);
+    newActivity = { ...newActivity, id: uid() };
+    setActivities([newActivity, ...activities]);
+    console.log(activities);
   }
 
   return <Form onAddActivity={handleAddActivity}></Form>;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,6 +6,11 @@ export default function Form({ onAddActivity }) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
+    if (data.isForGoodWeather) {
+      data.isForGoodWeather = true;
+    } else {
+      data.isForGoodWeather = false;
+    }
     e.target.reset();
     e.target.name.focus();
     onAddActivity(data);
@@ -15,7 +20,7 @@ export default function Form({ onAddActivity }) {
     <form aria-labelledby="formHeading" onSubmit={handleSubmit}>
       <h2 id="formHeading">Add new Activity:</h2>
       <label htmlFor="name">Name:</label>
-      <input name="name" id="name" type="text"></input>
+      <input name="name" id="name" type="text" required></input>
       <label htmlFor="isForGoodWeather">Good-weather activity:</label>
       <input
         type="checkbox"
