61149d19fb09f940cec27f97d5ffa7f917b7c222
Veliko <Kardziev>
2023-06-16 16:13:20 +0200

    update after Code Review

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 import Form from "./components/Form/Form.js";
-import useLocalStorageState from "use-local-storage-state";
-import { uid } from "uid";
 import List from "./components/List/List";
 
 function App() {
-  const [activities, setActivities] = useLocalStorageState("activities", {
-    defaultValue: [],
-  });
+	const [activities, setActivities] = useLocalStorageState("activities", {
+		defaultValue: [],
+	});
 
-  function handleAddActivity(newActivity) {
-    newActivity = { ...newActivity, id: uid() };
-    setActivities([newActivity, ...activities]);
-  }
+	function handleAddActivity(newActivity) {
+		newActivity = { ...newActivity, id: uid() };
+		setActivities([newActivity, ...activities]);
+	}
 
-  return (
-    <div className="App">
-      <List activities={activities} />
-      <Form onAddActivity={handleAddActivity} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<List activities={activities} />
+			<Form onAddActivity={handleAddActivity} />
+		</div>
+	);
 }
 
 export default App;
